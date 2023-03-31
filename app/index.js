5f47aa38530ab69fb44256ac954714a880987f34
Rokas <rookas.rudzenskas@gmail.com>
2023-03-31 18:02:24 +0300

    Add new file index.js with a React component that displays a text with the author's name

---
+++ app/index.js
@@ -0,0 +1,15 @@
+//@ts-nocheck
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+const Page = () => {
+  return (
+    <View>
+      <Text>
+        byrookas 🚀
+      </Text>
+    </View>
+  );
+};
+
+export default Page;
