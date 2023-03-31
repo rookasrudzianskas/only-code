d6664d0d674c27a1499695406573b335038d4b6c
Rokas <rookas.rudzenskas@gmail.com>
2023-03-31 19:13:45 +0300

    Refactor user profile page UI with new design and additional features

--- app/user/[id].js
+++ app/user/[id].js
@@ -1,7 +1,7 @@
 //@ts-nocheck
 import React from 'react';
-import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
-import {EvilIcons} from "@expo/vector-icons";
+import {Text, View, StyleSheet, TouchableOpacity, ImageBackground, Image} from 'react-native';
+import {AntDesign, EvilIcons, Feather, Ionicons} from "@expo/vector-icons";
 import {Link, useRouter, useSearchParams} from "expo-router";
 import users from '../../assets/data/users';
 
@@ -10,14 +10,58 @@ const ProfilePage = () => {
   const {id} = useSearchParams();
   const user = users.find((user) => user.id === id);
   return (
-    <View className="pt-16">
-      <Link href={'/'} asChild>
-        <TouchableOpacity className="absolute top-16 left-5" activeOpacity={0.7}>
-          <EvilIcons name="arrow-left" size={30} color="black" />
-        </TouchableOpacity>
-      </Link>
-      <View>
+    <View className="">
+      <ImageBackground className="h-56 w-full" style={{}} src={user.coverImage}>
+        <View style={{
+          ...StyleSheet.absoluteFillObject,
+          backgroundColor: 'rgba(0,0,0,0.5)',
+        }}/>
+      <View className="absolute top-16 right-5">
+        <Feather name="more-vertical" size={24} color="white" />
+      </View>
+      <View className="absolute flex flex-row items-center top-12 left-5">
+        <Link href={'/'} asChild>
+          <TouchableOpacity className="" activeOpacity={0.7}>
+            <EvilIcons name="arrow-left" size={30} color="white" />
+          </TouchableOpacity>
+        </Link>
 
+        <View className="flex flex-col pr-4">
+          <View className="flex flex-row items-center">
+            <Text className="text-white text-lg font-semibold mr-2 pl-2">OnlyCode</Text>
+            <Ionicons name="checkmark-circle-sharp" className="mt-4" size={14} color="white" />
+          </View>
+          <View className="flex flex-row pl-2">
+            <View className="flex flex-row items-center">
+              <Text className="text-white font-semibold">1.4</Text>
+              <Text className="text-white mx-1">Posts</Text>
+            </View>
+            <View className="flex flex-row items-center">
+              <Text className="text-white font-semibold">300.6K</Text>
+              <Text className="text-white mx-1">Likes</Text>
+            </View>
+            <View className="flex flex-row items-center">
+              <Text className="text-white font-semibold">1.3M</Text>
+              <Text className="text-white mx-1">Followers</Text>
+            </View>
+          </View>
+        </View>
+      </View>
+      </ImageBackground>
+
+      <View className="flex flex-row items-end -mt-12 justify-between mx-5">
+        <Image src={user.avatar} className="w-28 h-28 border-4 border-white rounded-full" />
+        <View className="flex flex-row items-center">
+          <TouchableOpacity className="w-9 h-9 mx-2 flex items-center justify-center border rounded-full border-gray-200" activeOpacity={0.7}>
+            <AntDesign name="sharealt" size={18} color="#4169e1" />
+          </TouchableOpacity>
+          <TouchableOpacity className="w-9 h-9 mx-2 flex items-center justify-center border rounded-full border-gray-200" activeOpacity={0.7}>
+            <EvilIcons name="star" size={18} color="#4169e1" />
+          </TouchableOpacity>
+          <TouchableOpacity className="w-9 h-9 mx-2 flex items-center justify-center border rounded-full border-gray-200" activeOpacity={0.7}>
+            <Feather name="share" size={18} color="#4169e1" />
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
