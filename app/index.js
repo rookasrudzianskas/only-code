7a3e6783fd6ed2f083a369269f442fed7084386d
Rokas <rookas.rudzenskas@gmail.com>
2023-04-07 19:38:54 +0300

    Add sign out button and functionality to header in index.js

--- app/index.js
+++ app/index.js
@@ -1,19 +1,26 @@
 //@ts-nocheck
 import React from 'react';
-import {Text, View, Image, ImageBackground, FlatList} from 'react-native';
+import {Text, View, Image, ImageBackground, FlatList, TouchableOpacity} from 'react-native';
 import users from '../assets/data/users'
 import UserCard from "../components/UserCard";
 import {Link} from "expo-router";
 import {AntDesign} from "@expo/vector-icons";
+import {useAuthenticator} from "@aws-amplify/ui-react-native";
 
 const Page = () => {
   const user = users[0];
+  const {signOut} = useAuthenticator()
 
   return (
     <View className="pt-16 mx-5">
-      <Link href={'/newPost'} className="">
-        <AntDesign name="addfile" size={20} className="my-2" color="black" />
-      </Link>
+      <View className="flex flex-row justify-between items-center">
+        <Link href={'/newPost'} className="">
+          <AntDesign name="addfile" size={20} className="my-2" color="black" />
+        </Link>
+        <TouchableOpacity activeOpacity={0.7} onPress={() => signOut()}>
+          <Text className="font-semibold">Sign Out</Text>
+        </TouchableOpacity>
+      </View>
       <FlatList
         contentContainerStyle={{paddingBottom: 100}}
         data={users}
