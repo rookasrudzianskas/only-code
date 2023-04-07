88b02f76403bb9c39d894f70c61aab6d5cf9ab06
Rokas <rookas.rudzenskas@gmail.com>
2023-04-07 19:24:23 +0300

    Configure AWS Amplify in the app's layout file to enable authentication and API access

--- app/_layout.js
+++ app/_layout.js
@@ -1,11 +1,13 @@
-
+import { Amplify } from 'aws-amplify';
 import React from 'react'
 import {Stack} from "expo-router";
+import awsconfig from '../src/aws-exports';
 
+Amplify.configure(awsconfig);
 
 
 export default function RootLayout() {
   return (
-    <Stack />
+    <Stack screenOptions={{ headerShown: false }} />
   )
 }
