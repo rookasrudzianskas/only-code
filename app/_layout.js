ca864594e7d6816b9d08cb6f7b98ad8b3a62e5b0
Rokas <rookas.rudzenskas@gmail.com>
2023-04-08 10:46:20 +0300

    Add user to database upon sign in using AWS Amplify GraphQL in RootLayout component

--- app/_layout.js
+++ app/_layout.js
@@ -1,8 +1,9 @@
-import { Amplify, Hub } from 'aws-amplify';
+import {Amplify, API, DataStore, Hub} from 'aws-amplify';
 import React, {useEffect} from 'react'
 import {Stack} from "expo-router";
 import awsconfig from '../src/aws-exports';
 import {Authenticator} from "@aws-amplify/ui-react-native";
+import {User} from "../src/models";
 
 Amplify.configure({
   ...awsconfig,
@@ -11,11 +12,35 @@ Amplify.configure({
   }
 });
 
+const CreateUserMutation = `
+  mutation createUser($input: CreateUserInput!) {
+	createUser(input: $input) {
+  	id
+    name
+    handle
+    bio
+    subscriptionPrice
+  }
+}
+`
+
 export default function RootLayout() {
 
   useEffect(() => {
-    const removeListener = Hub.listen('auth', ({payload: {event, data}}) => {
+    const removeListener = Hub.listen('auth', async ({payload: {event, data}}) => {
+      if (data.payload.event === 'signIn') {
+        const userInfo = data.payload.data.attributes
+        // Save the user to the database
+        const newUser = {
+          id: userInfo.sub,
+          name: userInfo.name,
+          handle: userInfo.nickname,
+          subscriptionPrice: 0,
+        }
 
+        await API.graphql({ query: CreateUserMutation, variables: { input: newUser }})
+        console.log('User created successfully')
+      }
     })
     return () => {
       removeListener()
